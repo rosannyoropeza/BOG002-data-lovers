11ebbf0051c6033702c92dccea845f8732715d1a
leysanchezb92 <leysanchezb92@hotmail.com>
2021-03-25 17:39:31 -0500

    Barra de busqueda, iconos de deportes
    Se agregan iconos de deportes, y barras de busqueda

--- src/data.js
+++ src/data.js
@@ -3,37 +3,56 @@
 /*export const example = () => {
   return 'example';
 };*/
+// import data  from './data/athletes/atletasImg.js';
 
-export const banderas = fetch("./data/banderas.json")
-  .then(function (response){
-    console.log(response);
-  return response.json()
-})
-.then(function(data){
-  //traer todas las banderas que tengan paises que inician con A
-  var result=data.filter(bandera=> bandera.NOC.startsWith("A"));  
-  for (let i = 0; i < result.length; i++) {
-      console.log(data[i].PAIS)
-  }
+// export const data= {
+//   deportes:function(sport){
+//     return fetch("./data/athletes/atletasImg.json")
+//       .then(function(response){
+//         return response.json()
+//       })
+//       .then(function(data){
+//         let newSport=[]
+//         data.forEach(function(deporte){
+//           newSport.push(deporte.sport)
+//         })
+//         const dataArr= new Set(newSport)
+//         let result= [...dataArr]
+//         return result
+//       })
+//   },
+// }
 
-  for (let i = 0; i < data.length; i++) {
+// export const banderas = fetch("./data/banderas.json")
+//   .then(function (response){
+//     console.log(response);
+//   return response.json()
+// })
+// .then(function(data){
+//   //traer todas las banderas que tengan paises que inician con A
+//   var result=data.filter(bandera=> bandera.NOC.startsWith("A"));  
+//   for (let i = 0; i < result.length; i++) {
+//       console.log(data[i].PAIS)
+//   }
+
+//   for (let i = 0; i < data.length; i++) {
 
 
-      let codigo_pais =  data[i].NOC;
-      let bandera_url =  data[i].URL;
-      let elem  = document.getElementById(codigo_pais)
+//       let codigo_pais =  data[i].NOC;
+//       let bandera_url =  data[i].URL;
+//       let elem  = document.getElementById(codigo_pais)
 
-      if (codigo_pais[0]=='A')
-      console.log(codigo_pais)
+//       if (codigo_pais[0]=='A')
+//       console.log(codigo_pais)
 
-      var imagen = document.createElement("img");
-      imagen.src = bandera_url;
-      imagen.width=32;
-      imagen.height=32;
+//       var imagen = document.createElement("img");
+//       imagen.src = bandera_url;
+//       imagen.width=32;
+//       imagen.height=32;
 
-      if (elem)
-      elem.prepend(imagen);
-  }
+//       if (elem)
+//       elem.prepend(imagen);
+//   }
 
-});;
+// });;
 
--- src/main.js
+++ src/main.js
@@ -1,68 +1,40 @@
 //import { data } from './data.js';
+
 // import data from './data/lol/lol.js';
- //import data  from './data/athletes/athletes.js';
- //import banderas from './data/banderas.json';
-
-//Aqui comienza el slider
-let slider=document.querySelector("#slider"); //Devuelve el primer elemento del documento que coincida con el grupo especificado de selectores.
-let imagenes=document.querySelectorAll(".slider_section");//devuelve una NodeList estática (no viva) que representa una lista de elementos del documento que coinciden con el grupo de selectores indicados.
-const btnLeft=document.querySelector("btn_left");
-const btnRigth=document.querySelector("btn_right");
-let sliderSectionLast=imagenes[imagenes.length -1];
-
-slider.insertAdjacentElement("afterbegin",sliderSectionLast);
-
-function next(){//Funcion para ir hacia adelante
-    let imagenes=document.querySelectorAll(".slider_section")[0];
-    slider.insertAdjacentElement("afterbegin", imagenes);//para analizar la cadena insertAdjacentElement +"Afterbegin" Justo dentro del elemento, antes de su primer elemento hijo.
-    slider.style.transition="all 0.8s";
-   
-    setTimeout(function(){
-        slider.style.transition="none";
-        slider.insertAdjacentElement('beforeend',imagenes);//para analizar la cadena insertAdjacentElement + beforeend Justo dentro del elemento, después de su último elemento hijo.
-        slider.style.marginleft="-100%";
-    }, 500);
+//import banderas from './data/banderas.json';
+// import slider from './slider'
+import data from "./data/athletes/atletasImg.js";
+console.log("data", data);
+function principal() {
+    pintarDeportes()
 }
-
-function prev(){//Funcion para ir atras
-    let imagenes=document.querySelectorAll(".slider_section");
-    let sliderSectionLast=imagenes[imagenes.length -1];
-    slider.style.marginleft = "0";
-    slider.style.transition="all 0.8s";
-    setTimeout(function(){
-        slider.style.transition="none";
-        slider.insertAdjacentElement('afterbegin',sliderSectionLast);
-        slider.style.marginleft="-100%";
-   }, 500);
-} 
-
-var ciclo = 0;
-function btn_right(){
-    clearInterval(ini);//Para limpiar el intervalo de tiempo del ciclo
-    next();
-    if(ciclo)
-    clearInterval(ciclo); //Para evitar que el ciclo se repita 
-    ciclo = setInterval(function(){//Para llamar a la funcion y que se ejecute de forma infitiva
-        next();
-    },3000);
+principal()
+function listaDeportes(){
+    let newSport = [];
+    data.forEach(function (deporte) {
+      if (!newSport.includes(deporte.sport)) {
+        newSport.push(deporte.sport);
+      }
+    });
+    return newSport
+}
+function pintarDeportes(){
+    let eventos=listaDeportes()
+    let contenido = "";
+    let logosDeportes = document.getElementById("logosDeportes");
+    eventos.forEach(function (disciplina) {
+        console.log(disciplina);
+        let imagen = '<img src="./assets/depOlimpicos/' + disciplina + '.svg"/>';
+    contenido = contenido + imagen + disciplina;
+    });
+    logosDeportes.innerHTML = contenido;
+}
+let buscarDeporte= document.getElementById('search-btn')
+buscarDeporte.addEventListener('click',function(){
+    const nombreDeporte = document.getElementById('search').value
+    let resultado=listaDeportes(nombreDeporte)
+    console.log('soy buscar',resultado)
     
-}
+})
 
-function btn_left(){
-    clearInterval(ini); //Para limpiar el intervalo de tiempo del ciclo
-        prev();
-
-   if(ciclo) //Para evitar que el ciclo se repita 
-   clearInterval(ciclo);
-}
-
-var ini = setInterval(function(){//Para llamar a la funcion y que se ejecute de forma infitiva
-    next();
-},3000);
-
-//Termina funcion slider
-
-import sport from './data/sports.json'
-
-console.log(sport)
 
